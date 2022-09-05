dd682b0ee963e86b6273e2158afe9a5104606cbe
el-maaroufi <74816091+elmaaroufi1@users.noreply.github.com>
2022-09-05 06:21:41 +0200

    setting movies

--- src/comopnents/movies/Movies.jsx
+++ src/comopnents/movies/Movies.jsx
@@ -1,9 +1,55 @@
 import './movies.css' 
-
+import logo from '../../assets/images/back2.png'
 const Movies = () => {
   return (
-    <div>
-      movies
+    <div className='movies'>
+      <div className='movies__item'>
+        <img src={logo} alt="" />
+        <h3>Movie Name</h3>
+         <p>
+           8.9
+         </p>
+      </div>
+
+      <div className='movies__item'>
+        <img src={logo} alt="" />
+        <h3>Movie Name</h3>
+         <p>
+           8.9
+         </p>
+      </div>
+
+      <div className='movies__item'>
+        <img src={logo} alt="" />
+        <h3>Movie Name</h3>
+         <p>
+           8.9
+         </p>
+      </div>
+
+      <div className='movies__item'>
+        <img src={logo} alt="" />
+        <h3>Movie Name</h3>
+         <p>
+           8.9
+         </p>
+      </div>
+
+      <div className='movies__item'>
+        <img src={logo} alt="" />
+        <h3>Movie Name</h3>
+         <p>
+           8.9
+         </p>
+      </div>
+
+      <div className='movies__item'>
+        <img src={logo} alt="" />
+        <h3>Movie Name</h3>
+         <p>
+           8.9
+         </p>
+      </div>
     </div>
   )
 }
