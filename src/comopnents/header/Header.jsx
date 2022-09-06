a71861da128360c24fa820757c7cf1d61d8a235e
el-maaroufi <74816091+elmaaroufi1@users.noreply.github.com>
2022-09-06 21:11:25 +0200

    add new features

--- src/comopnents/header/Header.jsx
+++ src/comopnents/header/Header.jsx
@@ -1,9 +1,12 @@
 import './header.css'
 import Search from '../search/Search'
+
 const Header = () => {
   return (
     <div className='header'>
         <Search/>
+        <h1><i class="fa-solid fa-angle-left"></i></h1>
+        <h1><i class="fa-solid fa-angle-right"></i></h1>
     </div>
   )
 }
